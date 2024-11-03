e1eaf723d2a2b12d1e055ce59818b1e9829fecd5
Shubham singh <rajputshubhamsingh593@gmail.com>
2024-11-03 17:55:34 +0530

    new one

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'dark-gray':'#1f242b',
+      }
+    },
+  },
+  plugins: [],
+}
+
